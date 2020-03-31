0b4dcd8e5b387a5a888e9621700e437b1cae5376
krikarthk <kkadapa@northwell.edu>
2020-03-30 22:58:53 -0400

    Updates

---
+++ precache-manifest.1dfcfc9387107e866d8c79e571a562c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "668e395ab25191efffe3b819243c869a",
+    "url": "/randstandupgenerator/index.html"
+  },
+  {
+    "revision": "1db7e52f5c5f12387f57",
+    "url": "/randstandupgenerator/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "720913d14756d38927ed",
+    "url": "/randstandupgenerator/static/css/main.75edb93e.chunk.css"
+  },
+  {
+    "revision": "1db7e52f5c5f12387f57",
+    "url": "/randstandupgenerator/static/js/2.b8637b68.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/randstandupgenerator/static/js/2.b8637b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "720913d14756d38927ed",
+    "url": "/randstandupgenerator/static/js/main.0a9215e6.chunk.js"
+  },
+  {
+    "revision": "90474dd1360e49b07d9f",
+    "url": "/randstandupgenerator/static/js/runtime-main.e5202e1d.js"
+  }
+]);
